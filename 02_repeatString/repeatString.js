f26f1383d8ac1c88893b811358235613d9e2a06d
Solomon <kingsolomontm@gmail.com>
2024-01-07 16:18:54 -0500

    repeatString function works and passes first 3 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    for (let i = 1; i <= num; i++){
+        return string.repeat(num);
+    }
 };
 
+repeatString('hey', 3);
 // Do not edit below this line
-module.exports = repeatString;
+// module.exports = repeatString;
