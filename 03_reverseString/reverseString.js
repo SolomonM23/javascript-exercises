7886fd7e49409fe80b99dd1f3e0eeebcd5412464
Solomon <kingsolomontm@gmail.com>
2024-01-10 19:35:07 -0500

    all tests pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = string;
 
+    return reversed.split("").reverse().join("");
 };
 
+console.log(reverseString('Built in, better'));
 // Do not edit below this line
 module.exports = reverseString;
