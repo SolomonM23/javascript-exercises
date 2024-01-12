ee11b5e10ccf9962495bf6d2079f915f5679ee9e
Solomon <kingsolomontm@gmail.com>
2024-01-12 15:37:18 -0500

    first test passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, find) {
+    let remove = array.indexOf(find);
+    let inputArr = array.splice(remove, 1);
 
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4] , 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
