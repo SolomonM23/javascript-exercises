7fffe3be0090a31f721eca0e379718655c070804
Solomon <kingsolomontm@gmail.com>
2024-01-17 15:56:14 -0500

    changed logic to compare two arrays for similar elements. does not pass test 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,32 @@
-const removeFromArray = function(array, find) {
-    let remove = array.indexOf(find);
-    let inputArr = array.splice(remove, 1);
+const removeFromArray = function(array, ...toRemove) {
+    // console.log(toRemove);
+    // console.log(array);
+    // console.log(array.includes(toRemove));// toRemove elements not found in arrays, even though they are there
+    let indexes = [];
 
-    return array;
+    for (let i = 0; i < array.length; i++){ //loops through elements of array
+        for (let j = 0; j < toRemove.length; j++){ //loops through elements of toRemove
+            if (array[i] === toRemove[j]){ //compares elements from each array to see if there is a match
+                return true;
+                // let remove = array.indexOf(toRemove);
+                // let inputArr = array.splice(remove, 1);
+            }
+        }
+    }
+    return false;
+
+
+
+    // for (let rems of toRemove){
+    //     let remove = array.indexOf(toRemove); //index value of toRemove elements
+    //     console.log(remove);
+    //     let inputArr = array.splice(remove, 1); //removed element values of toRemove elements
+    //     // console.log(inputArr);
+    // }
+    // return array;
 };
 
-console.log(removeFromArray([1, 2, 3, 4] , 4));
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 3, 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
