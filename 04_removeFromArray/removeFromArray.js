37712ae3f9716ccea20c54ae34e3692dff28c0f4
Solomon <kingsolomontm@gmail.com>
2024-01-20 15:25:58 -0500

    all tests pass.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,13 @@
 const removeFromArray = function(array, ...toRemove) {
-    // console.log(toRemove);
-    // console.log(array);
-    // console.log(array.includes(toRemove));// toRemove elements not found in arrays, even though they are there
-    let indexes = [];
 
-    for (let i = 0; i < array.length; i++){ //loops through elements of array
-        for (let j = 0; j < toRemove.length; j++){ //loops through elements of toRemove
-            if (array[i] === toRemove[j]){ //compares elements from each array to see if there is a match
-                return true;
-                // let remove = array.indexOf(toRemove);
-                // let inputArr = array.splice(remove, 1);
-            }
+    const indexArr = [];
+    array.forEach((item) => {
+        if (!toRemove.includes(item)) {
+            indexArr.push(item);    
         }
-    }
-    return false;
-
-
-
-    // for (let rems of toRemove){
-    //     let remove = array.indexOf(toRemove); //index value of toRemove elements
-    //     console.log(remove);
-    //     let inputArr = array.splice(remove, 1); //removed element values of toRemove elements
-    //     // console.log(inputArr);
-    // }
-    // return array;
-};
+    });
+    return indexArr;
+}   
 
 console.log(removeFromArray([1, 2, 3, 4, 5], 1, 3, 5));
 
