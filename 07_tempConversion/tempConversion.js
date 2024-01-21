5e8e6d0b939f2389c1575ca723940b5a40e407c9
Solomon <kingsolomontm@gmail.com>
2024-01-21 15:31:31 -0500

    optimized each function to be only one line

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
 const convertToCelsius = function(num) {
-  let convert = (num - 32) * 5/9;
-  return rounded = Math.round(convert * 10) / 10;
+  return Math.round((num - 32) * (5/9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(num) {
-  let convert = (num * 9/5) + 32;
-  return rounded = Math.round(convert * 10) / 10;
+  return Math.round(((num * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
